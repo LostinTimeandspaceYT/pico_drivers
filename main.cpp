d547025f44f08adadad7520b701164b0b666d923
Nathan <natelostintimeandspace@gmail.com>
2024-09-22 09:28:23 -0400

    Fixed bug in ush_read, allocated more buffer space
    Signed-off-by: Nathan <natelostintimeandspace@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include "i2c/i2c.hpp"
 #include "pico/stdlib.h"
-#include "ssd1306/bitmap.hpp"
 #include <cstdint>
 #include <stdio.h>
 #include <string.h>
@@ -22,6 +21,7 @@
 
 #if DEBUG_SSD1306
 #include "ssd1306/ssd1306.hpp"
+#include "ssd1306/bitmap.hpp"
 #endif
 
 /*Function Prototypes */
@@ -29,37 +29,30 @@ void setup(void);
 void forever_blink(void);
 void scan_i2c_bus();
 
-
 // working buffers allocations (size could be customized)
-#define BUF_IN_SIZE    32
-#define BUF_OUT_SIZE   32
-#define PATH_MAX_SIZE  32
+#define BUF_IN_SIZE 128
+#define BUF_OUT_SIZE 128
+#define PATH_MAX_SIZE 128
 
 static char ush_in_buf[BUF_IN_SIZE];
 static char ush_out_buf[BUF_OUT_SIZE];
 
-
-
 // microshell instance handler
 static struct ush_object ush;
 
-
 // non-blocking read interface
-static int ush_read(struct ush_object *self, char *ch)
-{
-    // should be implemented as a FIFO
-    if (stdio_get_until(ch, 1, PICO_ERROR_TIMEOUT)> 0) {
-        *ch = getchar();
-        return 1;
-    }
-    return 0;
+static int ush_read(struct ush_object *self, char *ch) {
+  // should be implemented as a FIFO
+  if (stdio_get_until(ch, BUF_IN_SIZE, PICO_ERROR_TIMEOUT) > 0) {
+    return 1;
+  }
+  return 0;
 }
 
 // non-blocking write interface
-static int ush_write(struct ush_object *self, char ch)
-{
-    // should be implemented as a FIFO
-    return (printf("%c", ch) == 1);
+static int ush_write(struct ush_object *self, char ch) {
+  // should be implemented as a FIFO
+  return (printf("%c", ch) == 1);
 }
 
 // I/O interface descriptor
@@ -70,138 +63,141 @@ static const struct ush_io_interface ush_iface = {
 
 // microshell descriptor
 static const struct ush_descriptor ush_desc = {
-    .io = &ush_iface,                           // I/O interface pointer
-    .input_buffer = ush_in_buf,                 // working input buffer
-    .input_buffer_size = sizeof(ush_in_buf),    // working input buffer size
-    .output_buffer = ush_out_buf,               // working output buffer
-    .output_buffer_size = sizeof(ush_out_buf),  // working output buffer size
-    .path_max_length = PATH_MAX_SIZE,           // path maximum length (stack)
-    .hostname = "Pico2",                      // hostname (in prompt)
+    .io = &ush_iface,                          // I/O interface pointer
+    .input_buffer = ush_in_buf,                // working input buffer
+    .input_buffer_size = sizeof(ush_in_buf),   // working input buffer size
+    .output_buffer = ush_out_buf,              // working output buffer
+    .output_buffer_size = sizeof(ush_out_buf), // working output buffer size
+    .path_max_length = PATH_MAX_SIZE,          // path maximum length (stack)
+    .hostname = "Pico2",                       // hostname (in prompt)
 };
 
 // toggle file execute callback
-static void toggle_exec_callback(struct ush_object *self, struct ush_file_descriptor const *file, int argc, char *argv[])
-{
-    // simple toggle led, without any arguments validation
-    gpio_put(PICO_DEFAULT_LED_PIN, !gpio_get(PICO_DEFAULT_LED_PIN));
+static void toggle_exec_callback(struct ush_object *self,
+                                 struct ush_file_descriptor const *file,
+                                 int argc, char *argv[]) {
+  // simple toggle led, without any arguments validation
+  gpio_put(PICO_DEFAULT_LED_PIN, !gpio_get(PICO_DEFAULT_LED_PIN));
 }
 
 // reboot cmd file execute callback
-static void reboot_exec_callback(struct ush_object *self, struct ush_file_descriptor const *file, int argc, char *argv[])
-{
+static void reboot_exec_callback(struct ush_object *self,
+                                 struct ush_file_descriptor const *file,
+                                 int argc, char *argv[]) {
 #if defined(ARDUINO_ARCH_ESP32)
-    ESP.restart();
+  ESP.restart();
 #elif defined(ARDUINO_ARCH_AVR)
-    void (*reset)(void) = 0;
-    reset();
+  void (*reset)(void) = 0;
+  reset();
 #else
-    ush_print(self, "error: reboot not supported...");
+  ush_print(self, "error: reboot not supported...");
 #endif
 }
 
 // set file execute callback
-static void set_exec_callback(struct ush_object *self, struct ush_file_descriptor const *file, int argc, char *argv[])
-{
-    // arguments count validation
-    if (argc != 2) {
-        // return predefined error message
-        ush_print_status(self, USH_STATUS_ERROR_COMMAND_WRONG_ARGUMENTS);
-        return;
-    }
+static void set_exec_callback(struct ush_object *self,
+                              struct ush_file_descriptor const *file, int argc,
+                              char *argv[]) {
+  // arguments count validation
+  if (argc != 2) {
+    // return predefined error message
+    ush_print_status(self, USH_STATUS_ERROR_COMMAND_WRONG_ARGUMENTS);
+    return;
+  }
 
-    // arguments validation
-    if (strcmp(argv[1], "1") == 0) {
-        // turn led on
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
-    } else if (strcmp(argv[1], "0") == 0) {
-        // turn led off
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
-    } else {
-        // return predefined error message
-        ush_print_status(self, USH_STATUS_ERROR_COMMAND_WRONG_ARGUMENTS);
-        return;
-    }
+  // arguments validation
+  if (strcmp(argv[1], "1") == 0) {
+    // turn led on
+    gpio_put(PICO_DEFAULT_LED_PIN, 1);
+  } else if (strcmp(argv[1], "0") == 0) {
+    // turn led off
+    gpio_put(PICO_DEFAULT_LED_PIN, 0);
+  } else {
+    // return predefined error message
+    ush_print_status(self, USH_STATUS_ERROR_COMMAND_WRONG_ARGUMENTS);
+    return;
+  }
 }
 
 // info file get data callback
-size_t info_get_data_callback(struct ush_object *self, struct ush_file_descriptor const *file, uint8_t **data)
-{
-    static const char *info = "Use MicroShell and make fun!\r\n";
+size_t info_get_data_callback(struct ush_object *self,
+                              struct ush_file_descriptor const *file,
+                              uint8_t **data) {
+  static const char *info = "Use MicroShell and make fun!\r\n";
 
-    // return pointer to data
-    *data = (uint8_t*)info;
-    // return data size
-    return strlen(info);
+  // return pointer to data
+  *data = (uint8_t *)info;
+  // return data size
+  return strlen(info);
 }
 
 // led file get data callback
-size_t led_get_data_callback(struct ush_object *self, struct ush_file_descriptor const *file, uint8_t **data)
-{
-    // read current led state
-    bool state = gpio_get(PICO_DEFAULT_LED_PIN);
-    // return pointer to data
-    *data = (uint8_t*)((state) ? "1\r\n" : "0\r\n");
-    // return data size
-    return strlen((char*)(*data));
+size_t led_get_data_callback(struct ush_object *self,
+                             struct ush_file_descriptor const *file,
+                             uint8_t **data) {
+  // read current led state
+  bool state = gpio_get(PICO_DEFAULT_LED_PIN);
+  // return pointer to data
+  *data = (uint8_t *)((state) ? "1\r\n" : "0\r\n");
+  // return data size
+  return strlen((char *)(*data));
 }
 
 // led file set data callback
-void led_set_data_callback(struct ush_object *self, struct ush_file_descriptor const *file, uint8_t *data, size_t size)
-{
-    // data size validation
-    if (size < 1)
-        return;
+void led_set_data_callback(struct ush_object *self,
+                           struct ush_file_descriptor const *file,
+                           uint8_t *data, size_t size) {
+  // data size validation
+  if (size < 1)
+    return;
 
-    // arguments validation
-    if (data[0] == '1') {
-        // turn led on
-      gpio_put(PICO_DEFAULT_LED_PIN, 1);
-    } else if (data[0] == '0') {
-        // turn led off
-      gpio_put(PICO_DEFAULT_LED_PIN, 0);
-    }
+  // arguments validation
+  if (data[0] == '1') {
+    // turn led on
+    gpio_put(PICO_DEFAULT_LED_PIN, 1);
+  } else if (data[0] == '0') {
+    // turn led off
+    gpio_put(PICO_DEFAULT_LED_PIN, 0);
+  }
 }
 
 // time file get data callback
-size_t time_get_data_callback(struct ush_object *self, struct ush_file_descriptor const *file, uint8_t **data)
-{
-    static char time_buf[16];
-    // read current time
-    long current_time = millis();
-    // convert
-    snprintf(time_buf, sizeof(time_buf), "%ld\r\n", current_time);
-    time_buf[sizeof(time_buf) - 1] = 0;
-    // return pointer to data
-    *data = (uint8_t*)time_buf;
-    // return data size
-    return strlen((char*)(*data));
+size_t time_get_data_callback(struct ush_object *self,
+                              struct ush_file_descriptor const *file,
+                              uint8_t **data) {
+  static char time_buf[16];
+  // read current time
+  long current_time = millis();
+  // convert
+  snprintf(time_buf, sizeof(time_buf), "%ld\r\n", current_time);
+  time_buf[sizeof(time_buf) - 1] = 0;
+  // return pointer to data
+  *data = (uint8_t *)time_buf;
+  // return data size
+  return strlen((char *)(*data));
 }
 
 // root directory files descriptor
-static const struct ush_file_descriptor root_files[] = {
-    {
-        .name = "info.txt",                     // info.txt file name
-        .description = NULL,
-        .help = NULL,
-        .exec = NULL,
-        .get_data = info_get_data_callback,
-    }
-};
+static const struct ush_file_descriptor root_files[] = {{
+    .name = "info.txt", // info.txt file name
+    .description = NULL,
+    .help = NULL,
+    .exec = NULL,
+    .get_data = info_get_data_callback,
+}};
 
 // bin directory files descriptor
 static const struct ush_file_descriptor bin_files[] = {
     {
-        .name = "toggle",                       // toggle file name
-        .description = "toggle led",            // optional file description
-        .help = "usage: toggle\r\n",            // optional help manual
-        .exec = toggle_exec_callback,           // optional execute callback
+        .name = "toggle",             // toggle file name
+        .description = "toggle led",  // optional file description
+        .help = "usage: toggle\r\n",  // optional help manual
+        .exec = toggle_exec_callback, // optional execute callback
     },
-    {
-        .name = "set",                          // set file name
-        .description = "set led",
-        .help = "usage: set {0,1}\r\n",
-        .exec = set_exec_callback
-    },
+    {.name = "set", // set file name
+     .description = "set led",
+     .help = "usage: set {0,1}\r\n",
+     .exec = set_exec_callback},
 };
 
 // dev directory files descriptor
@@ -211,8 +207,8 @@ static const struct ush_file_descriptor dev_files[] = {
         .description = NULL,
         .help = NULL,
         .exec = NULL,
-        .get_data = led_get_data_callback,      // optional data getter callback
-        .set_data = led_set_data_callback,      // optional data setter callback
+        .get_data = led_get_data_callback, // optional data getter callback
+        .set_data = led_set_data_callback, // optional data setter callback
     },
     {
         .name = "time",
@@ -242,7 +238,6 @@ static struct ush_node_object bin;
 
 // cmd commands handler
 static struct ush_node_object cmd;
-
 
 int main() {
 
@@ -274,10 +269,16 @@ void setup(void) {
   gpio_set_dir(PICO_DEFAULT_LED_PIN, true);
   gpio_put(PICO_DEFAULT_LED_PIN, 1);
 
-  //FIXME: Why is this causing a linker error?
   ush_init(&ush, &ush_desc);
 
-  ush_node_mount(&ush, "/", &root, NULL, 0);
+  ush_commands_add(&ush, &cmd, cmd_files, sizeof(cmd_files) / sizeof(cmd_files[0]));
+
+  // mount root directory (root must be first)
+  ush_node_mount(&ush, "/", &root, root_files, sizeof(root_files) / sizeof(root_files[0]));
+  // mount dev directory
+  ush_node_mount(&ush, "/dev", &dev, dev_files, sizeof(dev_files) / sizeof(dev_files[0]));
+  // mount bin directory
+  ush_node_mount(&ush, "/bin", &bin, bin_files, sizeof(bin_files) / sizeof(bin_files[0]));
 }
 
 void forever_blink(void) {
