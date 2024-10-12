35ad9f8b0200889c6235ef2d19a6cf502d1240f9
Nate <natelostintimeandspace@gmail.com>
2024-10-11 20:15:00 -0400

    Removed old debug code, added TODO list.
    Signed-off-by: Nate <natelostintimeandspace@gmail.com>

--- main.cpp
+++ main.cpp
@@ -11,31 +11,12 @@
 
 #define AIRCR_Register (*((volatile uint32_t*)(PPB_BASE + 0x0ED0C))) //for reboot
 
-/* Debug Symbols */
-#define DEBUG_AP33772 (false)
-#define DEBUG_STUSB4500 (false)
-#define DEBUG_SSD1306 (false)
-
-#if DEBUG_AP33772
-#include "ap33772/ap33772.hpp"
-#endif
-
-#if DEBUG_STUSB4500
-#include "stusb4500/stusb4500.hpp"
-#endif
-
-#if DEBUG_SSD1306
-#include "ssd1306/bitmap.hpp"
-#include "ssd1306/ssd1306.hpp"
-
-OLED oled = OLED(64, 128, true);
-#endif
-
 /*Function Prototypes */
 void setup(void);
-void forever_blink(void);
-void scan_i2c_bus();
-void scan_i2c_bus(uint sda, uint scl);
+
+
+//TODO: Move the working shell code to ush folder.
+//TODO: Organize current commands by mount point.
 
 // working buffers allocations (size could be customized)
 #define BUF_IN_SIZE 512
@@ -288,11 +269,6 @@ static struct ush_node_object cmd;
 
 int main() {
   setup();
-#if DEBUG_SSD1306
-  oled.draw_bitmap(0, 0, 40, 32, sun_with_clouds_40x32);
-  oled.print(0, 36, (uint8_t *)"Hello World!");
-  oled.show();
-#endif
 
   while (1) {
     ush_service(&ush);
@@ -319,36 +295,5 @@ void setup(void) {
   ush_node_mount(&ush, "/", &root, root_files,sizeof(root_files) / sizeof(root_files[0]));
   ush_node_mount(&ush, "/dev", &dev, dev_files, sizeof(dev_files) / sizeof(dev_files[0]));
   ush_node_mount(&ush, "/bin", &bin, bin_files, sizeof(bin_files) / sizeof(bin_files[0]));
-}
-
-void forever_blink(void) {
-  for (;;) {
-    gpio_put(PICO_DEFAULT_LED_PIN, 0);
-    sleep_ms(500);
-
-    gpio_put(PICO_DEFAULT_LED_PIN, 1);
-    sleep_ms(500);
-
-    tight_loop_contents();
-  }
-}
-
-
-void scan_i2c_bus() {
-  I2C i2c = I2C();
-  printf("\n I2C Bus Scan\n");
-  printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F \n");
-  for (int addr = 0; addr < (1 << 7); ++addr) {
-    if (addr % 16 == 0) printf("%02x ", addr);
-    int ret;
-    uint8_t rxdata;
-    if (reserved_addr(addr))
-      ret = PICO_ERROR_GENERIC;
-    else
-      ret = i2c.read_blocking(addr, &rxdata, 1, false);
-    printf(ret < 0 ? "." : "@");
-    printf(addr % 16 == 15 ? "\n" : "  ");
-  }
-  printf("Done with i2c scan...\n");
 }
 
